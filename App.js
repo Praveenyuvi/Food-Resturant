d0c9312c437634429d7845eb9bd660fe41aca19a
Praveenyuvi <58223995+Praveenyuvi@users.noreply.github.com>
2023-06-26 10:27:09 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Footer from './Footer';
+import Header from './Header';
+
+function App() {
+
+  return (
+    <div className='page-list'>
+      <div className='page'>
+        <Header/>
+        <Footer/>
+       </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <>
+    <footer>
+        CopyRight &copy; 2023
+    </footer>
+    </>
+  )
+}
+
+export default Footer
