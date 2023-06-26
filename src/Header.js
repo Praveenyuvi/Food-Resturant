aad1136f5b1485fd755a722e322de4b35b61b8be
praveen98 <p.praveen@intimetec.com>
2023-06-26 10:36:11 +0530

    updated file

---
+++ src/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <>
+        <header  className="header">
+             Food Court 
+        </header>
+        <nav class="side-nav">
+            <li> Dashboard </li>
+            <li> Dashboard </li>
+            <li> Dashboard </li>
+            <li> Dashboard </li>
+        </nav>
+    </>
+  )
+}
+
+export default Header
